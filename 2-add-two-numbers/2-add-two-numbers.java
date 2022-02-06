959ac9046cc7d6a1a451adab8ef79397450c0e79
JinAh Kwak <58837220+saySthAbout@users.noreply.github.com>
2022-02-06 22:09:42 +0900

    Time: 3 ms (61.27%), Space: 48.4 MB (5.62%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -11,37 +11,37 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode node = new ListNode(0);
-        ListNode result = node;
+        ListNode result = node; // 7 0 8 
         int carry = 0;
         
         while (l1 != null || l2 != null) {
-            int sum = carry;
+            int sum = carry; // 1
             
             if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+                sum += l1.val; // 3
+                l1 = l1.next; // null
             }
             
             if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
+                sum += l2.val; // 4 // sum: 8
+                l2 = l2.next; // null
             }
             
-            if (sum >= 10) {
-                node.next = new ListNode(sum - 10);
-                carry = 1;
+            if (sum >= 10) { // 8
+                node.next = new ListNode(sum - 10); // new ListNode(0)
+                carry = 1; //  올림수 발생
             } else {
-                node.next = new ListNode(sum);
+                node.next = new ListNode(sum); // new ListNode(8)
                 carry = 0;
             }
             
-            node = node.next;
+            node = node.next; // new ListNode(8)
         }
         
         if (carry == 1) {
             node.next = new ListNode(1);
         }
-        
+        // result 7 0 8
         return result.next;
     }
 }
