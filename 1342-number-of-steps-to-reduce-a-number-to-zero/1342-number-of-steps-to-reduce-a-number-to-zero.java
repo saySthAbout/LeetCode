80a57b86e998c6deea2d20c7a112ca673608d1f4
JinAh Kwak <58837220+saySthAbout@users.noreply.github.com>
2022-02-22 08:59:10 +0900

    Time: 1 ms (55.94%), Space: 40.6 MB (31.41%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,18 +1,19 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int step = 0;
-        while(num > 0){
-            if(num % 2 == 0){ // 짝수인 경우
-                num = num / 2;
-            }else{ // 홀수인 경우
-                num = num - 1;
+        int count = 0;
+        while(num>0){
+            if(num == 1) {
+                count ++;
+                break;
             }
-            step++;
+            if(num%2 == 0){
+                count ++;
+            }
+            else {
+                count+=2;
+            }
+            num/=2;
         }
-        return step;
-        // 짝수인지 홀수인지 판단
-        // 짝수라면 나누기 2
-        // 홀수라면 -1
-        // 0 과 같으면 반환
+        return count;
     }
 }
