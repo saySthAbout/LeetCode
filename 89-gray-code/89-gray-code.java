ef2f72293fbf828c92a2dce35c497fd4036bfd14
JinAh Kwak <58837220+saySthAbout@users.noreply.github.com>
2022-05-22 16:22:57 +0900

    Time: 11 ms (44.84%), Space: 55.8 MB (42.08%) - LeetHub

--- 89-gray-code/89-gray-code.java
+++ 89-gray-code/89-gray-code.java
@@ -1,23 +1,18 @@
-// Recursion + Bit manipulation solution
-// Time: O(2 ^ n), 1ms
-// Space: O(n) for call stack, O(2 ^ n) for ans, 34.4mb
 class Solution {
     public List<Integer> grayCode(int n) {
-        // Base case
-        if(n == 0) {
-            List<Integer> ans = new ArrayList<>();
-            ans.add(0);
-            return ans;
+        List<Integer> grayCode = new ArrayList<>();
+        grayCode.add(0);
+
+        if (n == 0)
+            return grayCode;
+
+        for (int i = 0; i < n; i++) {
+            int length = grayCode.size();
+            for (int j = length - 1; j >= 0; j--) {
+                grayCode.add(grayCode.get(j) | (1 << i) );
+            }
         }
-        
-        // grayCode(n) = grayCode(n - 1) + (1 << (n - 1) + reversed(grayCode(n - 1) ))
-        List<Integer> ans = grayCode(n - 1);
-        int base = 1 << (n - 1); // 01 << 2 -> 0100
-        int size = ans.size();
-        for(int i = size - 1; i >= 0; i--) {
-            ans.add(base + ans.get(i));  // 1 + the mirror of the grayCode(n - 1)
-        }
-        
-        return ans;
+
+        return grayCode;
     }
 }
