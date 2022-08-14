d295585d23efd9e5f2406b929787225a2fbdd5b5
JinAh Kwak <58837220+saySthAbout@users.noreply.github.com>
2022-08-14 16:42:58 +0900

    Time: 1 ms (100.00%), Space: 42.7 MB (84.16%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -1,17 +1,15 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        int result = 0;
-        int partLen = part.length();
-        StringBuilder truncatedStr = new StringBuilder();
-        result = s.indexOf(part);
-        while(result != -1){
-            System.out.println("지점1 s = " + s + ", part = " + part + ", result = " + result);
-            truncatedStr.append(s.substring(0, result)).append(s.substring(result+partLen));
-            s = truncatedStr.toString();
-            System.out.println("지점2 s = " + s + ", part = " + part + ", result = " + result);
-            result = s.indexOf(part);
-            truncatedStr.setLength(0);
+        
+        StringBuilder sb = new StringBuilder(s);
+        
+        int index = sb.indexOf(part);
+        
+        while (sb.length() != 0 && index != -1) {
+            sb.delete(index, index + part.length());
+            index = sb.indexOf(part);
         }
-        return s;
+        
+        return sb.toString();
     }
 }
